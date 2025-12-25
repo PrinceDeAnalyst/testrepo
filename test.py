daac5a391aa9f816a0bc187b1600e42dde3eefd7
PrinceDeAnalyst <129728831+PrinceDeAnalyst@users.noreply.github.com>
2025-12-25 11:26:20 +0800

    Create test.py
    Using markdown to create a table

---
+++ test.py
@@ -0,0 +1,8 @@
+## Execute as a markdown cell
+
+|Fruits | Quantity(KG) | Price(in Rupees) |
+| -------|------ |--------| 
+| Apple | 2 | 200 |
+| Orange | 5| 90 |
+| Mango | 3 | 80 |
+| Grapes | 4 | 800 |
